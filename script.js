0a23918685559e8623a1017aea82fe6eaacd01f3
bazzawill <69337381+Barry-Williams@users.noreply.github.com>
2021-11-15 05:28:03 +0000

    Moved script to head using event listner load
    Added description
    
    used TH for table lables (still need to colspan)

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function createTable(title,tableData) {
+      let tDiv = document.querySelector("#tableDiv");
       let table = document.createElement('table');
       let row = {};
       let cell = {};
-    
+      let header = {};
+
+      //header = table.createElement('th')
+      let th = document.createElement("th", colspan = 21);
+      let text = document.createTextNode(title)
+      th.appendChild(text)
+      //th.colspan = 21
+      table.appendChild(th)
+      //const hrow = header.insertRow(0)
+      //const hcell = hrow.insertCell(0)
+      //hcell.innerText = title
+      //hcell.colSpan = 21
       tableData.forEach(function(rowData) {
         row = table.insertRow(-1);
         rowData.forEach(function(cellData) {
@@ -13,9 +25,20 @@ function createTable(title,tableData) {
       const textnode = document.createTextNode(title)
       const titleDiv = document.createElement("div");
       titleDiv.appendChild(textnode)
-      document.body.appendChild(titleDiv) 
-      document.body.appendChild(table);
+      //tDiv.append(textnode) 
+      tDiv.append(table);
     }
+
+function myFunction(){
+        let x = document.getElementById("selector");
+        let text = "";
+        let i;
+        for (i = 0; i < x.length ;i++) {
+          text += x.elements[i].value + "<br>";
+        }
+        let textnode = document.createTextNode(text)
+        document.getElementById("results").appendChild(textnode)
+      }
 let adv = []
 
 for (let i = 0; i <= 20; i++){
@@ -48,6 +71,7 @@ for (let i = 1; i <= 20; i++){
     dis[i][j]=i<j?i:j
   }
 }
-
-createTable("With advantage",adv);
-createTable("With Disadvantage",dis)
+window.addEventListener("load", () => {
+  createTable("With advantage",adv);
+  createTable("With Disadvantage",dis)
+})
